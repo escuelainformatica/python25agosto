830c665582a886b8ea169cc51447e0fe69988bde
Jorge Castro <jorge.castro.c@gmail.com>
2021-08-25 18:37:53 -0400

    first

--- productos_app/views.py
+++ productos_app/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from productos_app.models import Producto
+
+
 # Create your views here.
 
 
@@ -10,27 +12,27 @@ from productos_app.models import Producto
 def formulario_sumar(request):
     # usar get cuando se carga el formulario la primera vez.
     # user post cuando se carga el formulario despues de que el usuario envio datos
-    if request.method=='GET':
-        num1=0
-        num2=0
+    if request.method == 'GET':
+        num1 = 0
+        num2 = 0
     else:
-        num1=int(request.POST['num1'])
-        num2=int(request.POST['num2'])
-    total=num1+num2
-    context={"num1":num1,"num2":num2,"total":total}
-    return render(request,'productos/formulario_sumar.html',context)
+        num1 = int(request.POST['num1'])
+        num2 = int(request.POST['num2'])
+    total = num1 + num2
+    context = {"num1": num1, "num2": num2, "total": total}
+    return render(request, 'productos/formulario_sumar.html', context)
 
 
 def listar(request):
-    productos=Producto.objects.all() # leer todos los datos desde la entidad producto
-    context={"productos":productos}
-    return render(request,'productos/listar.html',context)
+    productos = Producto.objects.all()  # leer todos los datos desde la entidad producto
+    context = {"productos": productos}
+    return render(request, 'productos/listar.html', context)
 
-
+# format antigua usando HttpResponse
 def index(request):
     template = loader.get_template('miapp/index.html')
     context = {
-        'paises':["chile","argentina","peru"],
-        'nombre':'hola mundo'
+        'paises': ["chile", "argentina", "peru"],
+        'nombre': 'hola mundo'
     }
     return HttpResponse(template.render(context, request))
